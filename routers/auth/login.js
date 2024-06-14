e42916203b912ef8be26d4be499c6de0d7a1b112
Chakkaravarthi K <chakkaravarthik99@gmail.com>
2024-06-15 02:58:08 +0530

    hhh

--- routers/auth/login.js
+++ routers/auth/login.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { userModel } from '../../db-utils/model.js';
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
+
 
 const loginrouter = express.Router();
 
@@ -11,13 +13,21 @@ loginrouter.post('/', async (req, res) => {
 
         if (userobj) {
            //logic to handle login
-           bcrypt.compare(userdata.password, userobj.password, function(err, result) {
+           bcrypt.compare(userdata.password, userobj.password, async function(err, result) {
             // result == true
+
+            const user = await userModel.findOne({email: userdata.email},{password:0,_id:0,__v:0})
+
+            const userobject = user.toObject();
+
+            //jwt token
+            var token = jwt.sign(userobject, process.env.JWT_SECRET);
+           
             if(err){
                 res.status(500).send({msg:'something went wrong'})
             }else{
                 if(result){
-                    res.status(200).send({msg:'user athenticated success', code:1});
+                    res.status(200).send({msg:'user athenticated success', code:1, token});
                 }else{
                     res.status(404).send({msg:'user credentials wrong', code:0})
                 }
